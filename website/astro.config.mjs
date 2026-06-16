b8ac86e937b2409f68aeeb20d61a18af2bbca806
Tien DeLong-Headley <headley01@allegheny.edu>
2026-06-16 17:54:56 -0400

    fix: navigation reference and mdx styling

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://allegheny-computer-science.github.io',
   base: '/cis.allegheny.edu',
-  integrations: [react()],
+  integrations: [react(), mdx()],
 });
