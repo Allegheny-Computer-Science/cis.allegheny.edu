75d34ef4a968964b8f43851aea3e1330254cfdb8
Tien DeLong-Headley <headley01@allegheny.edu>
2026-09-03 21:09:01 -0400

    fix: github pages deploy css-less bug

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -4,8 +4,11 @@ import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+const base = process.env.ASTRO_BASE ?? '';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cis.allegheny.edu',
+  base,
   integrations: [react(), mdx(), sitemap()],
 });
